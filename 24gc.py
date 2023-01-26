99f511e73172f7f4b47e0a01514dd8b2e45ab245
rhuang12234 <rhuang12234@gmail.com>
2023-01-26 13:45:24 -0800

    init

--- 24gc.py
+++ 24gc.py
@@ -4,14 +4,12 @@
 # Format the output for 2 decimal places
 
 dna = 'ACAGAGCCAGCAGATATACAGCAGATACTAT'
-def GCPercent(dna):
-	GCamount = 0
-	for base in dna:
-		if base  == 'G' or base == 'C':
-			GCamount += 1
-	GCPercent = (GCamount / len(dna))
-	print('{:.2f}'.format(GCPercent))
-GCPercent(dna)
+GCamount = 0
+for base in dna:
+	if base  == 'G' or base == 'C':
+		GCamount += 1
+GCPercent = (GCamount / len(dna))
+print('{:.2f}'.format(GCPercent))
 """
 python3 24gc.py
 0.42
