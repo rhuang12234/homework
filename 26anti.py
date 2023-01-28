584f8f9da96ceb3d0cb34f9a4471f9c809c7c6f3
rhuang12234 <rhuang12234@gmail.com>
2023-01-28 13:38:54 -0800

    init

--- 26anti.py
+++ 26anti.py
@@ -6,8 +6,17 @@
 # Variation: try this with the range() function and slice syntax
 
 dna = 'ACTGAAAAAAAAAAA'
-
-
+ReverseComplement = ""
+for DNA in dna[::-1]:
+	if DNA == "A":
+		ReverseComplement += "T"
+	elif DNA == "C":
+		ReverseComplement += "G"
+	elif DNA == "G":
+		ReverseComplement += "C"
+	elif DNA == "T":
+		ReverseComplement += "A"
+print(ReverseComplement)
 """
 python3 26anti.py
 TTTTTTTTTTTCAGT
