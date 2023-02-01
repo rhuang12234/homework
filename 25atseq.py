2d4f71681c7aba6a46024ceb65897cee84fca4bd
rhuang12234 <rhuang12234@gmail.com>
2023-02-01 10:24:56 -0800

    init

--- 25atseq.py
+++ 25atseq.py
@@ -7,9 +7,32 @@
 # Report the length, AT fraction, and sequence
 
 # Note: set random.seed() if you want repeatable random numbers
+import random
 
+size = 30
+percentAT = 0.6
 
+dna =''
+countAT = 0
+for i in range(size):
+	r = random.random()
+	if r < percentAT / 2:
+		dna += 'A'
+		countAT += 1
+	elif r < percentAT:
+		dna += 'T'
+		countAT += 1
+	else:
+		if random.random() < 0.5:
+			dna += 'C'
+		else:
+			dna += 'G'
+newpercentAT = countAT / size
+print(len(dna))
+print(newpercentAT)
+print(dna)
 """
 python3 25atseq.py
 30 0.6666666666666666 ATTACCGTAATCTACTATTAAGTCACAACC
 """
+
