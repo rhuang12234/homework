c13f366030dd0d3c790eb138954467648d22efd1
rhuang12234 <rhuang12234@gmail.com>
2023-02-09 13:02:54 -0800

    init

--- 30stats.py
+++ 30stats.py
@@ -6,8 +6,43 @@
 # Hint: use sys.argv to get the values from the command line
 
 # Note: you are not allowed to import any library except sys
+import sys
 
+container = []
+for stuff in sys.argv[1:]:
+	container.append(float(stuff))
 
+count = len(container)
+print('Count:', count)
+
+minimum = container[0]
+maximum = container[0]
+for numbers in container:
+	if numbers < minimum:
+		minimum = numbers
+	if numbers > maximum:
+		maximum = numbers
+print('Maximum:', maximum)
+print('Minimum', minimum)
+
+sum = 0
+for i in container:
+	sum += i
+	mean = sum / len(container)
+print('Mean', '{:.2}'.format(mean))
+
+differenceSquares = []
+for values in container:
+	differenceSquares.append((values-mean) ** 2)
+variance = 0
+for differences in differenceSquares:
+	variance += differences
+variance = variance / len(container)
+standardeviation = variance ** 0.5
+print('Standard Deviation:','{:.2}'.format(standardeviation))
+
+median = (len(container) + 1) / 2
+print('Median:', median)
 """
 python3 30stats.py 3 1 4 1 5
 Count: 5
