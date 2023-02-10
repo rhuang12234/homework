5b3a83097a042c48f7d5299a6a9361166ec748f2
rhuang12234 <rhuang12234@gmail.com>
2023-02-10 14:21:56 -0800

    init

--- 33birthday.py
+++ 33birthday.py
@@ -9,8 +9,20 @@
 
 # Variation: try making the calendar a list
 # Variation: try making the birthdays a list
+import sys
+import math
+box = []
+for stuff in sys.argv[1:]:
+	box.append(int(stuff))
 
+days = box[0]
+people = box[1]
 
+probability = 1
+for sims in range (people):
+	probability *= (days - sims) / days
+probability = 1 - probability
+print('{:.4}'.format(probability))
 """
 python3 33birthday.py 365 23
 0.571
