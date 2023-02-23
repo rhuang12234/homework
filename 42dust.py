27906534e7d3050bcb026def7d6abec115120994
rhuang12234 <rhuang12234@gmail.com>
2023-02-23 14:57:11 -0800

    init

--- 42dust.py
+++ 42dust.py
@@ -11,7 +11,50 @@
 # Hint: make up some fake sequences for testing
 
 # Note: don't worry about "centering" the entropy on the window (yet)
+import mcb185
+import sys
+import gzip
+import math
 
+def entropy_calculation(seq):
+
+	counts = [0, 0, 0, 0]
+
+	for base in seq:
+		if base == 'A':
+			counts[0] += 1
+		elif base == 'C':
+			counts[1] += 1
+		elif base == 'G':
+			counts[2] += 1
+		elif base == 'T':
+			counts[3] += 1
+	total = len(seq)
+	entropy = 0
+	for count in counts:
+		if count > 0:
+			frequency = count / total
+			entropy += -frequency * math.log2(frequency)
+	return entropy			
+
+def filter_genome(genome, window_size, entropy_threshold):
+	filtered_genome = ''
+	for i in range(len(genome) - window_size):
+		window = genome[i:i+window_size]
+		entropy = entropy_calculation(window)
+		if entropy >= entropy_threshold:
+			filtered_genome += window
+		else:
+			filtered_genome += 'N' * window_size
+	return filtered_genome
+	
+window_size = int(sys.argv[2])
+entropy_threshold = float(sys.argv[3])
+
+for defline, seq in mcb185.read_fasta(sys.argv[1]):
+	genome = seq[:60]
+	filtered_genome = filter_genome(genome, window_size, entropy_threshold)
+	print(filtered_genome)
 
 """
 python3 42dust.py ~/DATA/E.coli/GCF_000005845.2_ASM584v2_genomic.fna.gz 11 1.4
