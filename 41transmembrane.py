6a32a1c971d945b56e3c8adf957bc98887291a20
rhuang12234 <rhuang12234@gmail.com>
2023-02-23 20:17:01 -0800

    init

--- 41transmembrane.py
+++ 41transmembrane.py
@@ -20,8 +20,76 @@
 # Hint: create a function for KD calculation
 # Hint: create a function for hydrophobic alpha-helix
 # Hint: use the same function for both signal peptide and transmembrane
+import sys
+import gzip
+import mcb185
 
+def hydropathy(seq):
+	total = 0
+	for DNA in seq:
+		if DNA == 'A':
+			total += 1.8
+		elif DNA == 'C':
+			total += 2.5
+		elif DNA == 'D':
+			total += -3.5
+		elif DNA == 'E':
+			total += -3.5
+		elif DNA == 'F':
+			total += 2.8
+		elif DNA == 'G':
+			total += -0.4
+		elif DNA == 'H':
+			total += -3.2
+		elif DNA == 'I':
+			total += 4.5
+		elif DNA == 'K':
+			total += -3.9
+		elif DNA == 'L':
+			total += 3.8
+		elif DNA == 'M':
+			total += 1.9
+		elif DNA == 'N':
+			total += -3.5
+		elif DNA == 'P':
+			total += -1.6
+		elif DNA == 'Q':
+			total += -3.5
+		elif DNA == 'R':
+			total += -4.5
+		elif DNA == 'S':
+			total += -0.8
+		elif DNA == 'T':
+			total += -0.7
+		elif DNA == 'V':
+			total += 4.2
+		elif DNA == 'W':
+			total += -0.9
+		elif DNA == 'Y':
+			total += -1.3 
+		
+	return total/len(seq)
 
+def is_transmembrane(sequence):
+	signal_peptide = sequence[:30]
+	hydrophobic_region = sequence[30:41]
+	signal_kd = hydropathy(signal_peptide)
+	hydrophobic_kd = hydropathy(signal_peptide)
+	return signal_kd > 2.5 and hydrophobic_kd > 2.0
+
+with gzip.open(sys.argv[1], 'rt') as fp:
+	header = ''
+	sequence = ''
+	for line in fp:
+		line = line.strip()
+		if line.startswith('>'):
+			if header and is_transmembrane(sequence):
+				print(header)
+			header = line[1:]
+			sequence = ''
+		else:
+			sequence += line
+		
 """
 python3 41transmembrane.py ~/DATA/E.coli/GCF_000005845.2_ASM584v2_protein.faa.gz
 NP_414560.1 Na(+):H(+) antiporter NhaA [Escherichia coli str. K-12 substr. MG1655]
